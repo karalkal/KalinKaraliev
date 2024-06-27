33516134fbee706533ea2cc4537594d6dba984ff
karalkal <karalkal@yahoo.co.uk>
2024-06-27 06:45:15 +0100

    replaced Stadia_AlidadeSatellite map

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,8 +1,14 @@
 //   ----    TILE LAYERS and INIT    ----    //
-const Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-	maxZoom: 22,
-	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'jpg'
+// const Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+// 	minZoom: 0,
+// 	maxZoom: 20,
+// 	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// 	ext: 'jpg'
+// });
+
+const OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
 const Jawg_Terrain = L.tileLayer('https://tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
@@ -25,7 +31,10 @@ const OpenRailwayMap = L.tileLayer('https://{s}.tiles.openrailwaymap.org/standar
 let map = L.map("map", { layers: [OpenStreetMap_HOT] });
 
 const baseMaps = { 		// last one in list will be displayed by default on initial render
-	"Satelite (Stadia)": Stadia_AlidadeSatellite, "Terrain (Jawg Lab)": Jawg_Terrain, "General (OpenStreetMap)": OpenStreetMap_HOT
+	// "Satelite (Stadia)": Stadia_AlidadeSatellite, 
+	"Topographical (OpenTopoMap)": OpenTopoMap,
+	"Terrain (Jawg Lab)": Jawg_Terrain, 
+	"General (OpenStreetMap)": OpenStreetMap_HOT
 };
 
 // define extra layers
