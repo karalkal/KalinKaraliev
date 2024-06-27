a1e6eb9a2433bc3ff810a8e2dfe1764dabb55c3b
karalkal <karalkal@yahoo.co.uk>
2024-06-27 22:04:46 +0100

    replaced Stadia.AlidadeSatellite with Esri_WorldImagery

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,10 +1,7 @@
 //   ----    TILE LAYERS and INIT    ----    //
-// const Stadia_AlidadeSatellite = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-// 	minZoom: 0,
-// 	maxZoom: 20,
-// 	attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// 	ext: 'jpg'
-// });
+const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
 
 const OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 17,
@@ -31,9 +28,9 @@ const OpenRailwayMap = L.tileLayer('https://{s}.tiles.openrailwaymap.org/standar
 let map = L.map("map", { layers: [OpenStreetMap_HOT] });
 
 const baseMaps = { 		// last one in list will be displayed by default on initial render
-	// "Satelite (Stadia)": Stadia_AlidadeSatellite, 
+	"Satellite": Esri_WorldImagery,
 	"Topographical (OpenTopoMap)": OpenTopoMap,
-	"Terrain (Jawg Lab)": Jawg_Terrain, 
+	"Terrain (Jawg Lab)": Jawg_Terrain,
 	"General (OpenStreetMap)": OpenStreetMap_HOT
 };
 
